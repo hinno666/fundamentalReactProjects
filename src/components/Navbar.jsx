949dca63d7e50cbc5ce81e41f895f301165a9a29
Andrey <luffytaroyo@gmail.com>
2023-08-26 13:56:58 +0300

    create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import reactLogo from '../assets/react.svg'
+import { FaBars } from 'react-icons/fa'
+import { links } from '../data'
+import { useRef, useState } from 'react'
+
+const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false)
+    const linksContainerRef = useRef(null)
+    const linksRef = useRef(null)
+
+    const toggleLinks = () => {
+        setShowLinks(!showLinks);
+    }
+
+    const linksStyles = {
+        height: showLinks ? `${linksRef.current.getBoundingClientRect().height}px` : '0px'
+    }
+
+    return (
+        <header className="header">
+            <div className='container'>
+                <nav className="header__nav">
+                    <div className='nav-header'>
+                        <a href="https://react.dev" target="_blank" rel='noreferrer'>
+                            <img src={reactLogo} className="logo react" alt="React logo" />
+                        </a>
+                        <button className='nav-toggle' onClick={toggleLinks}><FaBars /></button>
+                    </div>
+                    <div className='nav__container' ref={linksContainerRef} style={linksStyles}>
+                        <ul className="nav__links" ref={linksRef} >
+                            {links.map((link) => {
+                                const { id, url, text } = link;
+                                return (
+                                    <li key={id}>
+                                        <a href={url}>{text}</a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Navbar;
---
+++ src/data.jsx
@@ -0,0 +1,22 @@
+export const links = [
+    {
+        id: 1,
+        url: '#project1',
+        text: 'project1',
+    },
+    {
+        id: 2,
+        url: '#project2',
+        text: 'project2',
+    },
+    {
+        id: 3,
+        url: '#project3',
+        text: 'project3',
+    },
+    {
+        id: 4,
+        url: '#project4',
+        text: 'project4',
+    },
+]
