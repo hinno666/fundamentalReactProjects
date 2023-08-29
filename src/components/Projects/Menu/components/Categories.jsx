b1aa5ec6fa9ddc45aa8b89378cf763ac4cee1583
Andrew Karpov <luffytaroyo@gmail.com>
2023-08-29 15:50:20 +0300

    added menu project

--- src/components/Projects/Menu/components/Categories.jsx
+++ src/components/Projects/Menu/components/Categories.jsx
@@ -1,5 +1,17 @@
-export const Categories = () => {
+import PropTypes from "prop-types";
+
+
+export const Categories = ({ categories, filterItems }) => {
   return (
-    <div>Categories</div>
+    <div className="btn__container">{categories.map((category, index) => {
+      return (
+        <button className="btn__filter" key={index} onClick={() => filterItems(category)}>{category}</button>
+      )
+    })}</div>
   )
+}
+
+Categories.propTypes = {
+  categories: PropTypes.array.isRequired,
+  filterItems: PropTypes.func.isRequired,
 }
--- src/components/Projects/Menu/components/MenuItem.jsx
+++ src/components/Projects/Menu/components/MenuItem.jsx
@@ -1,17 +1,32 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 export const MenuItem = ({ img, title, price, desc }) => {
+
+
+    const [showText, setShowText] = useState(true);
+
     return (
         <article className='menu__item'>
-        <img src={img} alt={title} className='img' />
-        <div className='item__info'>
-            <header>
-                <h5>{title}</h5>
-                <span className='item__price'>${price}</span>
-            </header>
-            <p className='item-text'>
-                {desc}
-            </p>
-        </div>
-    </article>
+            <img src={img} alt={title} className='img' />
+            <div className='item__info'>
+                <header>
+                    <h5>{title}</h5>
+                    <span className='item__price'>${price}</span>
+                </header>
+                <p className='item-text'>
+                    {showText ? `${desc.substring(0, 30)}...` : desc}
+                    <button onClick={() => setShowText(!showText)}>show</button>
+                </p>
+            </div>
+        </article>
     )
+}
+
+
+MenuItem.propTypes = {
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    desc: PropTypes.string,
 }
